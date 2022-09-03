72d8ebdf791d58b97f41fc1738a62c25df6c97c7
artaleal <artaleal@yandex.ru>
2022-09-03 19:54:03 +0300

    feature: js

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,30 @@
-const popup = document.querySelector ('.popup');
-const buttonEdit = document.querySelector ('.profile__button-edit');
-const buttonClose = document.querySelector ('.popup__close-button');
-const editForm = document.querySelector ('.popup__container');
-const getName = document.querySelector ('.profile__name');
-const getJob = document.querySelector ('.profile__description');
-let nameInput = document.querySelector ('.popup__input_name');
-let jobInput = document.querySelector ('.popup__input_job');
+const popup = document.querySelector('.popup');
+const buttonEdit = document.querySelector('.profile__button-edit');
+const buttonClose = document.querySelector('.popup__close-button');
+const editForm = document.querySelector('.popup__container');
+const getName = document.querySelector('.profile__name');
+const getJob = document.querySelector('.profile__description');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
 function openPopup() {
-  popup.classList.add ('.popup_opened');
+  popup.classList.add('popup_opened');
   nameInput.value = getName.textContent;
   jobInput.value = getJob.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove ('.popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   getName.textContent = `${nameInput.value}`;
-  getJob.textContent = `${nameInput.value}`;
+  getJob.textContent = `${jobInput.value}`;
   closePopup();
 }
 
-buttonEdit.addEventListener ('click', openPopup);
-buttonClose.addEventListener ('click', closePopup);
-editForm.addEventListener ('submit', formSubmitHandler);
-
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
+editForm.addEventListener('submit', formSubmitHandler);
 
