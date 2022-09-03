8d206ae83499d330e6126168e064e4adf6df6e61
artaleal <artaleal@yandex.ru>
2022-09-03 16:56:34 +0300

    feature: JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-console.log('hello');
+const popup = document.querySelector ('.popup');
+const buttonEdit = document.querySelector ('.profile__button-edit');
+const buttonClose = document.querySelector ('.popup__close-button');
+const editForm = document.querySelector ('.popup__container');
+const getName = document.querySelector ('.profile__name');
+const getJob = document.querySelector ('.profile__description');
+let nameInput = document.querySelector ('.popup__input_name');
+let jobInput = document.querySelector ('.popup__input_job');
+
+function openPopup() {
+  popup.classList.add ('.popup_opened');
+  nameInput.value = getName.textContent;
+  jobInput.value = getJob.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove ('.popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  getName.textContent = `${nameInput.value}`;
+  getJob.textContent = `${nameInput.value}`;
+  closePopup();
+}
+
+buttonEdit.addEventListener ('click', openPopup);
+buttonClose.addEventListener ('click', closePopup);
+editForm.addEventListener ('submit', formSubmitHandler);
+
+
