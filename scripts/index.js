a686a6d4fe8645a031feea129db4f86ebc3d9b49
artaleal <artaleal@yandex.ru>
2022-11-24 19:05:00 +0300

    check fo comments

--- scripts/index.js
+++ scripts/index.js
@@ -83,33 +83,6 @@ buttonAdd.addEventListener('click', () => {
   popupNewCard.open();
 });
 
-// const initialCards = [
-//   {
-//     name: 'Архыз',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//   },
-//   {
-//     name: 'Иваново',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//   }
-// ];
-
 // function makePopup(universalPopup) {
 //   const buttonClosePopup = universalPopup.querySelector(".popup__close-button");
 //   const popup = {
